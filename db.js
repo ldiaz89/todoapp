25c9f41cb87649dcd404d2c502ac7f84b0745c2c
ldiaz89 <lgdt66@gmail.com>
2021-05-05 17:03:54 -0400

    test change

--- db.js
+++ db.js
@@ -18,8 +18,8 @@ const proConfig = process.env.DATABASE_URL; //heroku addons
 
 const pool = new Pool({
   connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,});
+    process.env.NODE_ENV === "production" ? proConfig : devConfig});
 
 
 
-module.exports = pool;
+module.exports = pool
